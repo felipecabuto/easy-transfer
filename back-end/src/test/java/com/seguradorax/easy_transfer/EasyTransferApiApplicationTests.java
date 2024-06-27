e3c76f66d34c119581f2f3a4fa2b7d58f8742696
Felipe Cabuto <felipecabuto@gmail.com>
2024-06-26 22:41:56 -0300

    integrando back-end da API com front-end em Vue.js

---
+++ back-end/src/test/java/com/seguradorax/easy_transfer/EasyTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguradorax.easy_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
